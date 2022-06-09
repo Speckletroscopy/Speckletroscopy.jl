5340e973cb0c11709d72469a97665fb35fb336bc
Noah Green <greenspaceexplorer@gmail.com>
2022-06-09 18:14:56 -0400

    Files generated by PkgTemplates
    PkgTemplates version: 0.7.26

---
+++ src/Speckletroscopy.jl
@@ -0,0 +1,5 @@
+module Speckletroscopy
+
+# Write your package code here.
+
+end
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using Speckletroscopy
+using Test
+
+@testset "Speckletroscopy.jl" begin
+    # Write your tests here.
+end
